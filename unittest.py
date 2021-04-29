f11c0132bcc179738bb6e501f7ff8d4757482792
Aaron Rafter <raftera@oregonstate.edu>
2021-04-29 16:39:36 -0700

    h

---
+++ unittest.py
@@ -0,0 +1,8 @@
+import ('math.py')
+
+print("Beginning tests. If no outputs are made, there are no issues.")
+
+assert addition((14, 26)) == 40, "Should be 40"
+
+assert addition((1023, 2130)) == 3153, "Should be 3153"
+
